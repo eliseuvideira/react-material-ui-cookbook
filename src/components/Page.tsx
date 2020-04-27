5149dc17c244400510ad61e88d3ee12b5447dc62
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-27 20:19:25 -0300

    Hide on Scroll, fading the AppBar on and off, as scrolling

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   AppBar,
@@ -6,6 +6,7 @@ import {
   IconButton,
   Typography,
   Button,
+  Fade,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -24,24 +25,42 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Page = () => {
+  const [scroll, setScroll] = useState({ scrolling: false, scrollTop: 0 });
+  const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    setScroll({
+      scrollTop,
+      scrolling: scrollTop > scroll.scrollTop,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.flex}>
-            Title
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+      <Fade in={!scroll.scrolling}>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.flex}>
+              My Title
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Fade>
       <div className={classes.toolbarMargin} />
       <ul>
         {new Array(500).fill(null).map((_, i) => (
