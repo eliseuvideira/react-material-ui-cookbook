7ef4f357649d26ea4eac222008473f47a1e8d301
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-27 13:19:57 -0300

    using the justify property

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import { Chip } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,16 +21,64 @@ const Page = () => {
     <div className={classes.root}>
       <Grid container spacing={4}>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <Paper className={classes.paper}>
+            <Grid container justify="center">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <Paper className={classes.paper}>
+            <Grid container justify="center">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <Paper className={classes.paper}>
+            <Grid container justify="center">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <Paper className={classes.paper}>
+            <Grid container justify="center">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
     </div>
