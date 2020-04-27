fe2fed3cdb116e505b1aa423805b5f214edd1ff9
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-27 14:35:24 -0300

    Adding an AppBar & Fixed position AppBar

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,37 +1,53 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  flex: {
+    flex: 1,
   },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  toolbarMargin: theme.mixins.toolbar,
 }));
 
 const Page = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
-        </Grid>
-      </Grid>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.flex}>
+            Title
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.toolbarMargin} />
+      <ul>
+        {new Array(500).fill(null).map((_, i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
     </div>
   );
 };
