a99e17e11720d97496649817fb2f97d87253361b
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-27 22:44:24 -0300

    Testing Toolbar by passing an different button to the right

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ScrollTopButton from './ScrollTopButton';
 import Toolbar from './Toolbar';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -21,7 +22,14 @@ const Page = () => {
   return (
     <div className={classes.root}>
       <div id="back-to-top-anchor" />
-      <Toolbar title="My Toolbar" />
+      <Toolbar
+        title="My Toolbar"
+        RightButton={() => (
+          <Button color="secondary" variant="contained">
+            Logout
+          </Button>
+        )}
+      />
       <ul>
         {new Array(500).fill(null).map((_, i) => (
           <li key={i}>{i}</li>
