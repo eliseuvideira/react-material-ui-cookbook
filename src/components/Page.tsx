bb26b6a2465c522d6b3378351bc06d16061a01ea
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-30 21:23:18 -0300

    refactored components to a single component, since subdividing was adding complexity and subdividing into components should be done aftwards, not prior to the implementation

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,105 +1,157 @@
 import React, { useState } from 'react';
-import AppBar from './AppBar';
-import Drawer from './Drawer';
-import SettingsIcon from '@material-ui/icons/Settings';
-import HomeIcon from '@material-ui/icons/Home';
-import WebIcon from '@material-ui/icons/Web';
 import {
-  Typography,
-  Paper,
   makeStyles,
-  createStyles,
   Grid,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core';
-import { DRAWER_WIDTH } from '../utils/constants';
 import clsx from 'clsx';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import HomeIcon from '@material-ui/icons/Home';
+import SettingsIcon from '@material-ui/icons/Settings';
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    content: {
-      flexGrow: 1,
-      width: `calc(100% - ${theme.spacing(7) + 1}px)`,
-      marginLeft: theme.spacing(7) + 1,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
+const APP_NAME = 'My App';
+const DRAWER_WIDTH = 200;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: DRAWER_WIDTH,
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  appBarMenuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  drawer: {
+    width: DRAWER_WIDTH,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    overflowX: 'hidden',
+  },
+  drawerOpen: {
+    width: DRAWER_WIDTH,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(7) + 1,
     },
-    contentShift: {
-      marginLeft: DRAWER_WIDTH,
-      width: `calc(100% - ${DRAWER_WIDTH}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-  }),
-);
+  },
+  drawerToolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+  },
+  drawerBottomList: {
+    marginTop: 'auto',
+  },
+  drawerListItem: {
+    borderRadius: 16,
+  },
+}));
 
 const Page = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const [content, setContent] = useState('Home');
-  const [items] = useState([
-    {
-      key: 'Home',
-      text: 'Home',
-      icon: <HomeIcon />,
-      onClick: () => setContent('Home'),
-    },
-    {
-      key: 'Page 2',
-      text: 'Page 2',
-      icon: <WebIcon />,
-      onClick: () => setContent('Page 2'),
-    },
-    {
-      key: 'Page 3',
-      text: 'Page 3',
-      icon: <WebIcon />,
-      disabled: true,
-      onClick: () => setContent('Page 3'),
-    },
-    {
-      key: 'Page 4',
-      text: 'Page 4',
-      icon: <WebIcon />,
-      onClick: () => setContent('Page 4'),
-    },
-    {
-      key: 'Page 5',
-      text: 'Page 5',
-      icon: <WebIcon />,
-      hidden: true,
-      onClick: () => setContent('Page 5'),
-    },
-  ]);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <div>
-      <AppBar open={open} title="My App" onAppBarClick={() => setOpen(!open)} />
-      <Drawer
-        open={open}
-        switchDrawer={(value) => setOpen(value)}
-        onClose={() => setOpen(false)}
-        items={items}
-        bottomItems={[
-          {
-            key: 'Settings',
-            text: 'Settings',
-            icon: <SettingsIcon />,
-            onClick: () => setContent('Settings'),
-          },
-        ]}
-      />
-      <Grid
-        container
-        className={clsx(classes.content, { [classes.contentShift]: open })}
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, { [classes.appBarShift]: drawerOpen })}
       >
-        <Grid item xs={12}>
-          <Paper>
-            <Typography>{content}</Typography>
-          </Paper>
-        </Grid>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={() => setDrawerOpen(!drawerOpen)}
+            className={classes.appBarMenuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            {APP_NAME}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: drawerOpen,
+          [classes.drawerClose]: !drawerOpen,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: drawerOpen,
+            [classes.drawerClose]: !drawerOpen,
+          }),
+        }}
+      >
+        <Toolbar className={classes.drawerToolbar}>
+          <IconButton onClick={() => setDrawerOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List>
+          <ListItem button onClick={() => alert('Home')}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+        </List>
+        <List className={classes.drawerBottomList}>
+          <ListItem
+            button
+            onClick={() => alert('Settings')}
+            className={classes.drawerListItem}
+          >
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <Grid container>
+        <Grid item></Grid>
       </Grid>
     </div>
   );
