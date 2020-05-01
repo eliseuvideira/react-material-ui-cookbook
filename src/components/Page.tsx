4adde314c230e345eb4c995bb99fea44ffd2c375
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-30 22:31:23 -0300

    fixed some issues regarding css and layout

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -13,6 +13,7 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  Paper,
 } from '@material-ui/core';
 import clsx from 'clsx';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -25,7 +26,18 @@ const DRAWER_WIDTH = 200;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    paddingLeft: theme.spacing(7) + 1,
+    transition: theme.transitions.create(['width', 'margin', 'padding'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  rootShift: {
+    paddingLeft: DRAWER_WIDTH,
+    transition: theme.transitions.create(['width', 'margin', 'padding'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -67,9 +79,6 @@ const useStyles = makeStyles((theme) => ({
     }),
     overflowX: 'hidden',
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(7) + 1,
-    },
   },
   drawerToolbar: {
     display: 'flex',
@@ -80,8 +89,11 @@ const useStyles = makeStyles((theme) => ({
   drawerBottomList: {
     marginTop: 'auto',
   },
-  drawerListItem: {
-    borderRadius: 16,
+  borderRadius16: {
+    borderRadius: 5,
+  },
+  padding5: {
+    padding: theme.spacing(5),
   },
 }));
 
@@ -90,70 +102,87 @@ const Page = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, { [classes.appBarShift]: drawerOpen })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            onClick={() => setDrawerOpen(!drawerOpen)}
-            className={classes.appBarMenuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            {APP_NAME}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: drawerOpen,
-          [classes.drawerClose]: !drawerOpen,
-        })}
-        classes={{
-          paper: clsx({
+    <Grid
+      container
+      className={clsx(classes.root, { [classes.rootShift]: drawerOpen })}
+    >
+      <Grid item sm={12}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: drawerOpen,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              onClick={() => setDrawerOpen(!drawerOpen)}
+              className={classes.appBarMenuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              {APP_NAME}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: drawerOpen,
             [classes.drawerClose]: !drawerOpen,
-          }),
-        }}
-      >
-        <Toolbar className={classes.drawerToolbar}>
-          <IconButton onClick={() => setDrawerOpen(false)}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List>
-          <ListItem button onClick={() => alert('Home')}>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItem>
-        </List>
-        <List className={classes.drawerBottomList}>
-          <ListItem
-            button
-            onClick={() => alert('Settings')}
-            className={classes.drawerListItem}
-          >
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItem>
-        </List>
-      </Drawer>
-      <Grid container>
-        <Grid item></Grid>
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: drawerOpen,
+              [classes.drawerClose]: !drawerOpen,
+            }),
+          }}
+        >
+          <Toolbar className={classes.drawerToolbar}>
+            <IconButton onClick={() => setDrawerOpen(false)}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List>
+            <ListItem
+              button
+              onClick={() => alert('Home')}
+              className={classes.borderRadius16}
+            >
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </List>
+          <List className={classes.drawerBottomList}>
+            <ListItem
+              button
+              onClick={() => alert('Settings')}
+              className={classes.borderRadius16}
+            >
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItem>
+          </List>
+        </Drawer>
       </Grid>
-    </div>
+      <Grid item sm={12}>
+        <Grid container className={classes.padding5}>
+          <Grid item sm={12}>
+            <Paper>
+              <Typography>Page</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
