7d808777fe2f03fccb02611d1818f4ddc7aa3c3f
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-02 18:07:59 -0300

    added elevation on hover of paper and cssbaseline

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Paper, { PaperProps } from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import { CssBaseline } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,22 +16,58 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const HighlightPaper: React.FC<PropsWithChildren<PaperProps>> = ({
+  children,
+  elevation: propsElevation,
+  ...props
+}) => {
+  const [elevation, setElevation] = useState(propsElevation);
+  return (
+    <Paper
+      {...props}
+      elevation={elevation}
+      onMouseEnter={() => setElevation(5)}
+      onMouseLeave={() => setElevation(1)}
+    >
+      {children}
+    </Paper>
+  );
+};
+
+HighlightPaper.defaultProps = {
+  elevation: 1,
+};
+
+HighlightPaper.propTypes = {
+  children: PropTypes.any.isRequired,
+  elevation: PropTypes.number,
+};
+
 const Page = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <Grid container spacing={4}>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
       </Grid>
     </div>
