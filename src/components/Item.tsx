a31415054212fe9e4ccb183d08da4b6eb4653234
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-02 11:20:08 -0300

    removed non-used files

--- src/components/Item.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Grid, GridProps } from '@material-ui/core';
-
-const Item = (props: Omit<Omit<GridProps, 'container'>, 'item'>) => {
-  return <Grid item {...props} />;
-};
-
-export default Item;
