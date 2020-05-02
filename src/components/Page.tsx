7bddc88857b0390b0b17b19b7fbb40df78e2ecf1
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-02 13:51:10 -0300

    Lazy Loading an ExpansionPanelContent

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -47,48 +47,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Lorem = () => (
-  <>
-    <Typography paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies
-      nibh ut ipsum placerat, eget egestas leo imperdiet. Etiam consectetur
-      mollis ultrices. Fusce eu eros a dui maximus rutrum. Aenean at dolor eu
-      nunc ultricies placerat. Sed finibus porta sapien eget euismod. Donec eget
-      tortor non turpis hendrerit euismod. Phasellus at commodo augue. Maecenas
-      scelerisque augue at mattis pharetra. Aenean fermentum sed neque id
-      feugiat.
-    </Typography>
-
-    <Typography paragraph>
-      Aliquam erat volutpat. Donec sit amet venenatis leo. Nullam tincidunt diam
-      in nisi pretium, sit amet tincidunt nisi aliquet. Proin quis justo
-      consectetur, congue nisi nec, pharetra erat. Ut volutpat pulvinar neque
-      vitae vestibulum. Phasellus nisl risus, dapibus at sapien in, aliquam
-      tempus tellus. Integer accumsan tortor id dolor lacinia, et pulvinar est
-      porttitor. Mauris a est vitae arcu iaculis dictum. Sed posuere suscipit
-      ultricies. Vivamus a lacus in dui vehicula tincidunt.
-    </Typography>
-
-    <Typography paragraph>
-      In ut velit laoreet, blandit nisi id, tempus mi. Mauris interdum in turpis
-      vel tempor. Vivamus tincidunt turpis vitae porta dignissim. Quisque
-      condimentum augue arcu, quis tincidunt erat luctus sit amet. Sed quis
-      ligula malesuada, sollicitudin nisl nec, molestie tellus. Donec commodo
-      consequat gravida. Mauris in rhoncus tellus, eget posuere risus.
-      Pellentesque eget lectus lorem. Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit. Integer condimentum, sapien varius vulputate lobortis,
-      urna elit vestibulum ligula, sit amet interdum lectus augue ac eros.
-      Vestibulum lorem ante, tincidunt eget faucibus id, placerat non est.
-      Vivamus pretium consectetur nunc at imperdiet. Nullam eu elit dui. In
-      imperdiet magna ac dui aliquam gravida. Aenean ipsum ex, fermentum eu
-      pretium quis, posuere et velit.
-    </Typography>
-  </>
-);
+const fetchPanelContent = async (index: number) =>
+  new Promise<string>((resolve) =>
+    setTimeout(
+      () =>
+        resolve(
+          [
+            'First Panel Content...',
+            'Second Panel Content...',
+            'Third Panel Content...',
+            'Fourth Panel Content...',
+          ][index],
+        ),
+      1000,
+    ),
+  );
 
 const Page = () => {
   const classes = useStyles();
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [panels, setPanels] = useState<{ title: string; content?: string }[]>([
+    { title: 'First Panel Title...' },
+    { title: 'Second Panel Title...' },
+    { title: 'Third Panel Title...' },
+    { title: 'Fourth Panel Title...' },
+  ]);
+  const onChange = (index: number) => async () => {
+    if (!panels[index].content) {
+      const content = await fetchPanelContent(index);
+      const newPanels = [...panels];
+      newPanels[index] = { ...newPanels[index], content };
+      setPanels(newPanels);
+    }
+  };
+
   const toolbar = (
     <Toolbar>
       <IconButton
@@ -131,30 +123,14 @@ const Page = () => {
       <Grid container className={classes.container}>
         <Grid item sm={12}>
           <Paper square className={classes.content}>
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                First
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails className={classes.panelDetails}>
-                <Lorem />
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                Second
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails className={classes.panelDetails}>
-                <Lorem />
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                Third
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails className={classes.panelDetails}>
-                <Lorem />
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
+            {panels.map((panel, index) => (
+              <ExpansionPanel key={index} onChange={onChange(index)}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography>{panel.title}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>{panel.content}</ExpansionPanelDetails>
+              </ExpansionPanel>
+            ))}
           </Paper>
         </Grid>
       </Grid>
