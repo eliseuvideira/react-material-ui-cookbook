76e04d361d47546a3e411307062fee09860e99ef
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-02 20:12:05 -0300

    visually selecting list items by switching icons

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -9,6 +9,8 @@ import {
   ListItemIcon,
 } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -16,26 +18,50 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const MaybeSelectedIcon: React.FC<{
+  selected: boolean;
+  Icon: () => JSX.Element;
+}> = ({ selected, Icon }) => (selected ? <CheckCircleOutlineIcon /> : <Icon />);
+
+MaybeSelectedIcon.propTypes = {
+  selected: PropTypes.bool.isRequired,
+  Icon: PropTypes.any.isRequired,
+};
+
 const Page = () => {
   const classes = useStyles();
-  const [users] = useState([
-    { name: 'John Doe' },
-    { name: 'Joseph Smith' },
-    { name: 'Janice Patterson' },
+  const [users, setUsers] = useState([
+    { name: 'John Doe', selected: false },
+    { name: 'Joseph Smith', selected: false },
+    { name: 'Janice Patterson', selected: false },
   ]);
+
+  const onClick = (index: number) => () => {
+    const items = [...users];
+    items[index] = { ...items[index], selected: !items[index].selected };
+    setUsers(items);
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <Grid container spacing={4}>
-        <Grid item xs={12}>
+        <Grid item xs={6}>
           <List>
             {users.map((user, index) => (
-              <ListItem key={index} button>
-                <ListItemIcon>
-                  <AccountCircleIcon />
-                </ListItemIcon>
+              <ListItem
+                key={index}
+                button
+                selected={user.selected}
+                onClick={onClick(index)}
+              >
                 <ListItemText primary={user.name} />
+                <ListItemIcon>
+                  <MaybeSelectedIcon
+                    selected={user.selected}
+                    Icon={() => <AccountCircleIcon />}
+                  />
+                </ListItemIcon>
               </ListItem>
             ))}
           </List>
