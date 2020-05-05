ae7675828783f2fd39506f3918eed60706e14ab7
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-05 01:11:46 -0300

    Added CardMedia to display images

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import {
   CssBaseline,
+  Grid,
   Card,
   CardContent,
   Typography,
   CardHeader,
-  Avatar,
-  CardActions,
-  IconButton,
+  CardMedia,
 } from '@material-ui/core';
-import PersonIcon from '@material-ui/icons/Person';
-import ContactMailIcon from '@material-ui/icons/ContactMail';
-import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import grapeFruit from '../assets/images/grapefruit-slice-332-332.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +24,10 @@ const useStyles = makeStyles((theme) => ({
   actions: {
     justifyContent: 'flex-end',
   },
+  media: {
+    height: 332,
+    width: 'auto',
+  },
 }));
 
 const Page = () => {
@@ -38,30 +38,15 @@ const Page = () => {
       <Grid container>
         <Grid item xs={12}>
           <Card className={classes.card}>
-            <CardHeader
-              title="Ron Swanson"
-              subheader="Legend"
-              avatar={
-                <Avatar>
-                  <PersonIcon />
-                </Avatar>
-              }
+            <CardHeader title="Grapefruit" subheader="Red" />
+            <CardMedia
+              className={classes.media}
+              image={grapeFruit}
+              title="Grapefruit"
             />
             <CardContent>
-              <Typography variant="caption">Joined 2009</Typography>
-              <Typography>
-                Some filler text about the user. There doesn&apos;t have to be a
-                lot - just enough so that the text span at least two lines.
-              </Typography>
+              <Typography>Mmmm. Grapefruit.</Typography>
             </CardContent>
-            <CardActions className={classes.actions} disableSpacing>
-              <IconButton>
-                <ContactMailIcon />
-              </IconButton>
-              <IconButton>
-                <ContactPhoneIcon />
-              </IconButton>
-            </CardActions>
           </Card>
         </Grid>
       </Grid>
