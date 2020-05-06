5e2de9fa6fa53569380f05b6b824a4981411802a
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-06 15:01:21 -0300

    Controlling Snackbar visibility using state

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Snackbar } from '@material-ui/core';
+import { CssBaseline, Snackbar, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -10,10 +10,28 @@ const useStyles = makeStyles({
 
 const Page = () => {
   const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+
+  const showSnackbar = () => {
+    setOpen(true);
+  };
+  const hideSnackbar = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Snackbar open message="Test" ContentProps={{ variant: 'elevation' }} />
+      <Button variant="contained" onClick={showSnackbar}>
+        Show Snackbar
+      </Button>
+      <Snackbar
+        open={open}
+        onClose={hideSnackbar}
+        autoHideDuration={5000}
+        message="Visible Snackbar!"
+      />
     </div>
   );
 };
