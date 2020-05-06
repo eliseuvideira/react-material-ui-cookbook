419596de732d99da68c1b71aa71a45225259cdd1
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-06 15:59:32 -0300

    Snackbar transitions

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Snackbar, Button } from '@material-ui/core';
+import {
+  CssBaseline,
+  Snackbar,
+  Button,
+  SnackbarProps,
+  Slide,
+  Grow,
+  Fade,
+  Grid,
+} from '@material-ui/core';
+import PropTypes from '../PropTypes';
 
 const useStyles = makeStyles({
   root: {
@@ -8,29 +18,92 @@ const useStyles = makeStyles({
   },
 });
 
+const CustomSnackbar: React.FC<
+  {
+    transition: 'slide' | 'grow' | 'fade';
+    direction?: 'left' | 'right' | 'down' | 'up';
+  } & SnackbarProps
+> = ({ transition, direction, ...props }) => (
+  <Snackbar
+    TransitionComponent={
+      {
+        slide: Slide,
+        grow: Grow,
+        fade: Fade,
+      }[transition]
+    }
+    TransitionProps={{ direction } as any}
+    {...props}
+  />
+);
+
+CustomSnackbar.propTypes = {
+  transition: PropTypes.any,
+  direction: PropTypes.any,
+};
+
 const Page = () => {
   const classes = useStyles();
 
-  const [open, setOpen] = useState(false);
-
-  const showSnackbar = () => {
-    setOpen(true);
-  };
-  const hideSnackbar = () => {
-    setOpen(false);
-  };
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+  const [fourth, setFourth] = useState(false);
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Button variant="contained" onClick={showSnackbar}>
-        Show Snackbar
-      </Button>
-      <Snackbar
-        open={open}
-        onClose={hideSnackbar}
+      <Grid container spacing={8}>
+        <Grid item>
+          <Button variant="contained" onClick={() => setFirst(true)}>
+            Slide Down
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={() => setSecond(true)}>
+            Slide Up
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={() => setThird(true)}>
+            Grow
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={() => setFourth(true)}>
+            Fade
+          </Button>
+        </Grid>
+      </Grid>
+      <CustomSnackbar
+        open={first}
+        onClose={() => setFirst(false)}
         autoHideDuration={5000}
-        message="Visible Snackbar!"
+        message="Slide Down"
+        transition="slide"
+        direction="down"
+      />
+      <CustomSnackbar
+        open={second}
+        onClose={() => setSecond(false)}
+        autoHideDuration={5000}
+        message="Slide Up"
+        transition="slide"
+        direction="up"
+      />
+      <CustomSnackbar
+        open={third}
+        onClose={() => setThird(false)}
+        autoHideDuration={5000}
+        message="Grow"
+        transition="grow"
+      />
+      <CustomSnackbar
+        open={fourth}
+        onClose={() => setFourth(false)}
+        autoHideDuration={5000}
+        message="Fade"
+        transition="fade"
       />
     </div>
   );
