5246f18f1d0f52d1ed00af2c55828edf96814833
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-07 20:55:34 -0300

    after merge, returning to master

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,67 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  CssBaseline,
-  Snackbar,
-  Button,
-  IconButton,
-  Typography,
-} from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Paper, { PaperProps } from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import { CssBaseline } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-});
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const HighlightPaper: React.FC<PropsWithChildren<PaperProps>> = ({
+  children,
+  elevation: propsElevation,
+  ...props
+}) => {
+  const [elevation, setElevation] = useState(propsElevation);
+  return (
+    <Paper
+      {...props}
+      elevation={elevation}
+      onMouseEnter={() => setElevation(5)}
+      onMouseLeave={() => setElevation(1)}
+    >
+      {children}
+    </Paper>
+  );
+};
+
+HighlightPaper.defaultProps = {
+  elevation: 1,
+};
+
+HighlightPaper.propTypes = {
+  children: PropTypes.any.isRequired,
+  elevation: PropTypes.number,
+};
 
 const Page = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
   return (
     <div className={classes.root}>
-      <BrowserRouter>
-        <CssBaseline />
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <Button variant="contained" onClick={() => setOpen(true)}>
-              Create
-            </Button>
-          )}
-        />
-        <Route
-          exact
-          path="/thing"
-          render={() => <Typography>Thing</Typography>}
-        />
-        <Snackbar
-          open={open}
-          onClose={() => setOpen(false)}
-          message="All done doing the thing"
-          action={[
-            <Button
-              key="more"
-              color="secondary"
-              component={Link}
-              to="/thing"
-              onClick={() => setOpen(false)}
-            >
-              More
-            </Button>,
-            <IconButton
-              key="close"
-              color="inherit"
-              onClick={() => setOpen(false)}
-            >
-              <CloseIcon />
-            </IconButton>,
-          ]}
-        />
-      </BrowserRouter>
+      <CssBaseline />
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
