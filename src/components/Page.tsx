776fa73c6f6e78a6f042ee5a36d7059a24a62ab3
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-07 21:47:26 -0300

    edge cases when buttons are rendered inside already colored backgrounds, how to adapt according to context

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Grid, Button, Typography } from '@material-ui/core';
+import {
+  CssBaseline,
+  Grid,
+  Button,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,14 +18,76 @@ const useStyles = makeStyles((theme) => ({
   container: {
     margin: theme.spacing(1),
   },
+  marginLeftAuto: {
+    marginLeft: 'auto',
+  },
+  buttons: {
+    marginLeft: theme.spacing(1),
+  },
 }));
 
 const Page = () => {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(false);
+  const [appBarColor, setAppBarColor] = useState<
+    'default' | 'primary' | 'secondary'
+  >('default');
+
+  const getNext = (
+    color: 'default' | 'primary' | 'secondary',
+  ): 'default' | 'primary' | 'secondary' => {
+    if (color === 'default') {
+      return 'primary';
+    }
+    if (color === 'primary') {
+      return 'secondary';
+    }
+    return 'default';
+  };
+
+  const onChangeAppBarColors = () => {
+    setAppBarColor(getNext(appBarColor));
+  };
+
+  const buttonColor = (
+    color: 'default' | 'primary' | 'secondary',
+  ): 'default' | 'primary' | 'secondary' => {
+    return getNext(color);
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
+      <AppBar position="fixed" color={appBarColor}>
+        <Toolbar>
+          <IconButton color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">My App</Typography>
+          <Button
+            variant="text"
+            color="inherit"
+            className={classes.marginLeftAuto}
+          >
+            Text
+          </Button>
+          <Button
+            variant="outlined"
+            color="inherit"
+            className={classes.buttons}
+          >
+            Outlined
+          </Button>
+          <Button
+            variant="contained"
+            color={buttonColor(appBarColor)}
+            className={classes.buttons}
+          >
+            Contained
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
       <Grid
         container
         spacing={2}
@@ -90,7 +161,10 @@ const Page = () => {
           </Grid>
         </Grid>
         <Grid item>
-          <Button onClick={() => setDisabled(!disabled)}>Switch</Button>
+          <Button onClick={() => setDisabled(!disabled)}>
+            Switch Disabled
+          </Button>
+          <Button onClick={onChangeAppBarColors}>Switch AppBar Colors</Button>
         </Grid>
       </Grid>
     </div>
