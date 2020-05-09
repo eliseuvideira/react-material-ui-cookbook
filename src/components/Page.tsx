73590ead229a6e6473bc2c2284773951db4e3679
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-09 18:41:05 -0300

    Using InputAdornment as validation indicator

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -10,6 +10,9 @@ import {
 } from '@material-ui/core';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,16 +46,70 @@ const PasswordField: React.FC<Omit<TextFieldProps, 'type'>> = (props) => {
   );
 };
 
+const ValidationField: React.FC<
+  { isValid: (value: string) => boolean; value: string } & TextFieldProps
+> = ({ isValid, value, ...props }) => {
+  const empty = value === '';
+  const valid = isValid(value);
+  let startAdornment: any;
+
+  if (empty) {
+    startAdornment = null;
+  } else {
+    if (valid) {
+      startAdornment = (
+        <InputAdornment position="start">
+          <CheckCircleIcon color="primary" />
+        </InputAdornment>
+      );
+    } else {
+      startAdornment = (
+        <InputAdornment position="start">
+          <ErrorIcon color="error" />
+        </InputAdornment>
+      );
+    }
+  }
+
+  return (
+    <TextField
+      {...props}
+      error={!empty && !valid}
+      InputProps={{ startAdornment }}
+    />
+  );
+};
+
+ValidationField.propTypes = {
+  isValid: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
 const Page = () => {
   const classes = useStyles();
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Grid container spacing={4} className={classes.container}>
+      <Grid
+        container
+        spacing={4}
+        className={classes.container}
+        direction="column"
+      >
+        <Grid item>
+          <ValidationField
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            isValid={(v) => /\S+@\S+\.\S+/.test(v)}
+          />
+        </Grid>
         <Grid item>
           <PasswordField
+            label="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
