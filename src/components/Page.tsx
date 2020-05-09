f635d9c6cddc43019db845cf14ac4344ce4143d2
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-08 21:41:55 -0300

    after merge, returning to master

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,34 +1,73 @@
-import React from 'react';
+import React, { useState, PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Grid, Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Paper, { PaperProps } from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import { CssBaseline } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-});
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const HighlightPaper: React.FC<PropsWithChildren<PaperProps>> = ({
+  children,
+  elevation: propsElevation,
+  ...props
+}) => {
+  const [elevation, setElevation] = useState(propsElevation);
+  return (
+    <Paper
+      {...props}
+      elevation={elevation}
+      onMouseEnter={() => setElevation(5)}
+      onMouseLeave={() => setElevation(1)}
+    >
+      {children}
+    </Paper>
+  );
+};
+
+HighlightPaper.defaultProps = {
+  elevation: 1,
+};
+
+HighlightPaper.propTypes = {
+  children: PropTypes.any.isRequired,
+  elevation: PropTypes.number,
+};
 
 const Page = () => {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Grid container direction="column" spacing={4}>
-        <Grid item>
-          <Button variant="contained" size="small" color="primary">
-            Add
-          </Button>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
-        <Grid item>
-          <Button variant="contained" size="medium" color="primary">
-            Add
-          </Button>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
-        <Grid item>
-          <Button variant="contained" size="large" color="primary">
-            Add
-          </Button>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <HighlightPaper className={classes.paper}>
+            xs=12 sm=6 md=3
+          </HighlightPaper>
         </Grid>
       </Grid>
     </div>
