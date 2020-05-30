87deaa1ed749dd48321c65267063cf53e6d87057
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-30 12:42:19 -0300

    using an switch group

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,15 +6,10 @@ import {
   FormControlLabel,
   Container,
   FormLabel,
-  Radio,
-  RadioGroup,
+  FormGroup,
+  Switch,
 } from '@material-ui/core';
-import CarOutlinedIcon from '@material-ui/icons/DirectionsCarOutlined';
-import CarIcon from '@material-ui/icons/DirectionsCar';
-import BusOutlinedIcon from '@material-ui/icons/DirectionsBusOutlined';
-import BusIcon from '@material-ui/icons/DirectionsBus';
-import TrainOutlinedIcon from '@material-ui/icons/TrainOutlined';
-import TrainIcon from '@material-ui/icons/Train';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
   root: {
@@ -22,60 +17,56 @@ const useStyles = makeStyles({
   },
 });
 
+const SwitchGroup: React.FC<{
+  label: string;
+  onChange: (
+    index: number,
+  ) => (event: React.ChangeEvent<HTMLInputElement>) => void;
+  values: { checked?: boolean; label: string }[];
+}> = ({ values, label, onChange }) => (
+  <FormControl component="fieldset">
+    <FormLabel component="legend">{label}</FormLabel>
+    <FormGroup>
+      {values.map((value, index) => (
+        <FormControlLabel
+          key={index}
+          control={
+            <Switch checked={value.checked} onChange={onChange(index)} />
+          }
+          label={value.label}
+        />
+      ))}
+    </FormGroup>
+  </FormControl>
+);
+
+SwitchGroup.propTypes = {
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  values: PropTypes.array.isRequired,
+};
+
 const Page = () => {
   const classes = useStyles();
 
-  const [value, setValue] = useState('train');
+  const [values, setValues] = useState([
+    { label: 'First', checked: false },
+    { label: 'Second', checked: false },
+    { label: 'Third', checked: false },
+  ]);
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+  const onChange = (index: number) => () => {
+    const newValues = [...values];
+    const value = newValues[index];
+    newValues[index] = { ...value, checked: !value.checked };
+    setValues(newValues);
   };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <Container>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Travel Mode</FormLabel>
-          <RadioGroup name="travel" value={value} onChange={onChange} row>
-            <FormControlLabel
-              value="car"
-              control={
-                <Radio
-                  color="primary"
-                  icon={<CarOutlinedIcon />}
-                  checkedIcon={<CarIcon />}
-                />
-              }
-              label="Car"
-              labelPlacement="bottom"
-            />
-            <FormControlLabel
-              value="bus"
-              control={
-                <Radio
-                  color="primary"
-                  icon={<BusOutlinedIcon />}
-                  checkedIcon={<BusIcon />}
-                />
-              }
-              label="Bus"
-              labelPlacement="bottom"
-            />
-            <FormControlLabel
-              value="train"
-              control={
-                <Radio
-                  color="primary"
-                  icon={<TrainOutlinedIcon />}
-                  checkedIcon={<TrainIcon />}
-                />
-              }
-              label="Train"
-              labelPlacement="bottom"
-            />
-          </RadioGroup>
-        </FormControl>
+        <SwitchGroup label="Choices" values={values} onChange={onChange} />
       </Container>
     </div>
   );
